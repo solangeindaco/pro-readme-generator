33fc82d5fe15c5335fedec5ba3001ca897727c77
SolangeI <solange.indaco@gmail.com>
2023-09-28 11:06:47 -0700

    Added questions to the user

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the title your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Give a description of your project',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the steps required to install your project?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide instructions and examples for use.',
+    },
+    {
+      type: 'input',
+      name: 'credits',
+      message: 'Who were your collaborators?',
+    },
+    {
+      type: 'input',
+      name: 'license',
+      message: 'Which license do you use?',
+    },
+  ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    promptUser()
+    .then((answers) => writeFile('README.md', generateMarkdown(answers)))
+    .then(() => console.log('Successfully wrote to README.md'))
+    .catch((err) => console.error(err));
+}
 
 // Function call to initialize app
 init();
